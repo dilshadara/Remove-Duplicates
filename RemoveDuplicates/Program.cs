a8052a6c1afaf1297872915275b9a39fb1b83a9d
Dilshad <Dilshad>
2019-04-24 14:21:03 +1000

    duplicate remove

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveDuplicate();
+        }
+
+        static void RemoveDuplicate()
+        {
+            List<int> UnsortedList = new List<int> { 12, 11, 12, 21, 41, 43, 21 };
+
+            for (var i = 0; i < UnsortedList.Count; i++)
+            {
+                for (var j = i + 1; j < UnsortedList.Count; j++)
+                {
+                    if (UnsortedList[i] == UnsortedList[j])
+                    {
+                        UnsortedList.RemoveAt(j);
+
+                    }
+                }
+
+            }
+
+            foreach (var val in UnsortedList)
+            {
+                Console.WriteLine(val);
+            }
+            Console.Read();
+        }
+    }
+}
